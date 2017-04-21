843d447efb6fb566692652d904dea9d0002e5ab9
Denis Polozhiy <dpolozhiy@dataart.com>
2017-04-21 20:47:34 +0300

    Added displaying by category, worked on layout

--- TaskManagerApp/Scripts/spa/main.js
+++ TaskManagerApp/Scripts/spa/main.js
@@ -9,19 +9,41 @@ function Task(isDone, category, description) {
 function TaskViewModel() {
     var self = this;
 
-    self.categories = ["Home", "Work", "Business", "Health", "Sport", "Hobbie"];
+    self.categories = ["All", "Home", "Work", "Business", "Health", "Sport", "Hobbie"];
+    self.currentCategory = ko.observable();
 
     self.tasks = ko.observableArray([]);
 
     self.newTask = ko.observable();
     self.newTaskCategory = ko.observable();
 
+    self.filteredCategories = ko.computed(function() {
+        return ko.utils.arrayFilter(self.categories, function (item) {
+            if (item != "All")
+                return item;
+        });
+    });
+
+    self.chosenTasks = ko.computed(function () {
+        return ko.utils.arrayFilter(self.tasks(), function (item) {
+            if (self.currentCategory() == "All" || item.category == self.currentCategory())
+                return item;
+        });
+    });
+
+    self.changeCategory = function (item) {
+        self.currentCategory(item);
+    };
+
     self.addTask = function() {
         self.tasks.push(new Task(false, self.newTaskCategory(), self.newTask()));
     };
 
     self.removeTask = function(task) {
-        self.tasks.remove(task);
+        var i = self.tasks.indexOf(task);
+        if (i != -1) {
+            self.tasks.splice(i, 1);
+        }
     };
 
     self.saveTasks = function () {
@@ -34,10 +56,10 @@ function TaskViewModel() {
 
     $.getJSON("/api/Task", function (allData) {
         var mappedTasks = $.map($.parseJSON(allData), function (item) { return new Task(item.IsDone, item.Category, item.Description) });
-        console.log(mappedTasks);
         self.tasks(mappedTasks);
     });
 
+    self.changeCategory('All');
 }
 
 ko.applyBindings(new TaskViewModel());
